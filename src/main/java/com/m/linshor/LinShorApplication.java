840e28f8cdefd6883fbdfba3f66a5f0f1b39d3c4
Matthew Rusakov <112853687+MattWay224@users.noreply.github.com>
2025-03-14 18:43:23 +0300

    spring init

---
+++ src/main/java/com/m/linshor/LinShorApplication.java
@@ -0,0 +1,13 @@
+package com.m.linshor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinShorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinShorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/m/linshor/LinShorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m.linshor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinShorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
